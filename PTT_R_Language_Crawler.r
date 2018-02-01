074a49d10b5edf9f74efcb92d4b653e9ae3703eb
dong945 <dong.tw@gmail.com>
2018-02-01 21:31:32 +0800

    Add files via upload

---
+++ PTT_R_Language_Crawler.r
@@ -0,0 +1,38 @@
+# 抓取PTT
+library(XML)
+library(RCurl)
+
+
+# only for windows, MAC or Linux預設採OpenSSL
+# 因為網址為https, 要取用OpenSSL的SSL憑證檔(C:/R/R-3.4.1/library/RCurl/CurlSSL/cacert.pem)
+signatures <- system.file("CurlSSL", cainfo="cacert.pem", package="RCurl")
+
+#### Get the last page Number
+# 原來用"//div[@class='btn-group pull-right']/a", 但PTT改了
+# lastpage <- unlist(xpathSApply(htmlParse( getURL(paste0("https://www.ptt.cc/bbs/CVS/index.html"), cainfo = signatures)),  "//div[@class='btn-group pull-right']/a",xmlGetAttr, "href"))[[2]]
+# 變更為"//div[@class='btn-group btn-group-paging']/a"
+lastpage <- unlist(xpathSApply(htmlParse( getURL(paste0("https://www.ptt.cc/bbs/R_Language/index.html"), cainfo = signatures)),  "//div[@class='btn-group btn-group-paging']/a",xmlGetAttr, "href"))[[2]]
+lastpage <- gsub(".*index", "", lastpage)
+lastpage <- as.numeric(gsub("[.]*html", "", lastpage))+1
+lastpage  # 最新一筆的序號(2018/01/28為1732)
+
+#### 組出要抓取的網址, 這裏選用最新的100筆網址
+link.R <- NULL
+for( i in (lastpage-99):lastpage){
+  url <- paste0("https://www.ptt.cc/bbs/R_Language/index", i, ".html")
+  html <- htmlParse(getURL(url, cainfo = signatures))
+  url.list <- xpathSApply(html, "//div[@class='title']/a[@href]", xmlAttrs)
+  link.R <- c(link.R, paste('https://www.ptt.cc', url.list, sep=''))
+  print(paste("Get url from the billboard's(R Language) page :", i))
+}
+
+
+#### Write a function to save documents
+getdoc <- function(link, path){
+  doc <- xpathSApply(htmlParse(getURL(link, cainfo = signatures), encoding="UTF-8"), "//div[@id='main-content']", xmlValue)
+  name <- strsplit(link, '/')[[1]][6]
+  write(doc, file=file.path(path,gsub('html', 'txt', name)))
+}
+
+#### Set the path where you want to save documents
+system.time(sapply(1:10, function(i) getdoc(link.R[i], path="C:/RDATA/PTT/R_Language/")))
